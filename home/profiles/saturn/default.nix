29b91f99bf3c66489f1bb3da5a4530895fa52be0
AltF2 <contact@altf2.dev>
2023-01-04 21:04:55 +0100

    ref: move gnome-terminal seperate
    Signed-off-by: AltF2 <contact@altf2.dev>

--- home/profiles/saturn/default.nix
+++ home/profiles/saturn/default.nix
@@ -2,18 +2,6 @@
   imports = [
     ../../programs
     ../../wayland
+    ../../terminals/gnome-terminal.nix
   ];
-
-  programs.gnome-terminal = {
-    enable = true;
-
-    profile."acd6d090-49c7-4709-be81-8cedb4207b07" = {
-      cursorShape = "ibeam";
-      customCommand = "zsh";
-      default = true;
-      font = "MesloLGS NF 11";
-      loginShell = true;
-      visibleName = "p10k";
-    };
-  };
 }
---
+++ home/terminals/gnome-terminal.nix
@@ -0,0 +1,14 @@
+{
+  programs.gnome-terminal = {
+    enable = true;
+
+    profile."acd6d090-49c7-4709-be81-8cedb4207b07" = {
+      cursorShape = "ibeam";
+      customCommand = "zsh";
+      default = true;
+      font = "MesloLGS NF 11";
+      loginShell = true;
+      visibleName = "p10k";
+    };
+  };
+}
