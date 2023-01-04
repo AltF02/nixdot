cba29553211c5a6c12878584a2576a71b0aac2d3
AltF2 <contact@altf2.dev>
2023-01-04 16:57:26 +0100

    remove wait-online
    Signed-off-by: AltF2 <contact@altf2.dev>

--- modules/minimal.nix
+++ modules/minimal.nix
@@ -67,6 +67,13 @@
   # don't touch this
   system.stateVersion = lib.mkDefault "22.11";
 
+  # Don't wait for network startup
+  # https://old.reddit.com/r/NixOS/comments/vdz86j/how_to_remove_boot_dependency_on_network_for_a
+  systemd = {
+    targets.network-online.wantedBy = pkgs.lib.mkForce []; # Normally ["multi-user.target"]
+    services.NetworkManager-wait-online.wantedBy = pkgs.lib.mkForce []; # Normally ["network-online.target"]
+  };
+
   time.timeZone = lib.mkDefault "Europe/Amsterdam";
 
   users.users.matt = {
