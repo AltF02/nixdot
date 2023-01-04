e2021c2135e288c5d057514a02b062bf4478983e
AltF2 <contact@altf2.dev>
2023-01-04 19:56:22 +0100

    update zsh config
    Signed-off-by: AltF2 <contact@altf2.dev>

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -23,15 +23,23 @@
         file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
       }
       {
-        name = "zsh-you-should-use";
+        name = "you-should-use";
         src = pkgs.zsh-you-should-use;
       }
     ];
     oh-my-zsh = {
       enable = true;
-      plugins = ["git" "kubectl" "zsh-you-should-use"];
-      theme = "powerlevel10k/powerlevel10k";
+      plugins = ["git" "kubectl"];
     };
+
+    initExtra = ''
+      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+      # Initialization code that may require console input (password prompts, [y/n]
+      # confirmations, etc.) must go above this block; everything else may go below.
+      if [[ -r "${config.xdg.cacheHome}/p10k-instant-prompt-$\{(%):-%n}.zsh" ]]; then
+        source "${config.xdg.cacheHome}/p10k-instant-prompt-$\{(%):-%n}.zsh"
+      fi
+    '';
 
     shellAliases = {
       cat = "bat -pp";
