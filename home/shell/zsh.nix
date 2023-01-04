5f72192f035bb61d988bd98f4b7d3e00bd9495dd
AltF2 <contact@altf2.dev>
2023-01-04 20:00:54 +0100

    configure p10k
    Signed-off-by: AltF2 <contact@altf2.dev>

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -39,6 +39,9 @@
       if [[ -r "${config.xdg.cacheHome}/p10k-instant-prompt-$\{(%):-%n}.zsh" ]]; then
         source "${config.xdg.cacheHome}/p10k-instant-prompt-$\{(%):-%n}.zsh"
       fi
+
+      # To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
+      [[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
     '';
 
     shellAliases = {
