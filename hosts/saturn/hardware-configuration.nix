2743ddf72bd309fdbaca6db1c4ea3b6c80985973
AltF2 <contact@altf2.dev>
2023-01-05 19:44:09 +0100

    Revert "feat: saturn shrink tmpfs to 512M"
    This reverts commit 0d63ae306f14c51556853f99a952b9c6227d63d1.

--- hosts/saturn/hardware-configuration.nix
+++ hosts/saturn/hardware-configuration.nix
@@ -20,7 +20,7 @@
   fileSystems."/" = {
     device = "none";
     fsType = "tmpfs";
-    options = ["defaults" "size=512M" "mode=755"];
+    options = ["defaults" "size=2G" "mode=755"];
   };
 
   fileSystems."/boot" = {
