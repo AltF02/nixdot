093fee01c963cb1ee1d6a59ff95b6ec93444bd42
AltF2 <contact@altf2.dev>
2023-01-15 13:55:37 +0100

    feat: add git lfs for files
    Signed-off-by: AltF2 <contact@altf2.dev>

--- home/programs/files/config/wireplumber/bluetooth.lua.d/51-bluez-config.lua
+++ home/programs/files/config/wireplumber/bluetooth.lua.d/51-bluez-config.lua
@@ -1,6 +1,3 @@
-bluez_monitor.properties = {
-  ["bluez5.enable-sbc-xq"] = true,
-  ["bluez5.enable-msbc"] = true,
-  ["bluez5.enable-hw-volume"] = true,
-  ["bluez5.headset-roles"] = "[ hsp_hs hsp_ag hfp_hf hfp_ag ]"
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:7d5c69118c6339f716aece1afd3f4b5e245d581a2a73ee58e3e6d487a5b9a03d
+size 199
