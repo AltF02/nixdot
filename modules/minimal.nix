ea57faeae29030c713118a3930befc5d89bc8c89
Matthew <contact@altf2.dev>
2023-01-18 13:22:58 +0100

    feat: add busybox
    Signed-off-by: Matthew <contact@altf2.dev>

--- modules/minimal.nix
+++ modules/minimal.nix
@@ -7,6 +7,8 @@
 }:
 # configuration shared by all hosts
 {
+  environment.systemPackages = [pkgs.busybox];
+
   # enable zsh autocompletion for system packages (systemd, etc)
   environment.pathsToLink = ["/share/zsh"];
 
