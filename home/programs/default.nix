93b0ef6d202b471ec70b21549a65262d7c238149
Matthew <contact@altf2.dev>
2023-01-21 01:38:49 +0100

    fix: gpg add cache ttl
    Signed-off-by: Matthew <contact@altf2.dev>

--- home/programs/default.nix
+++ home/programs/default.nix
@@ -37,6 +37,14 @@
       enable = true;
       enableSshSupport = true;
       enableExtraSocket = true;
+
+      defaultCacheTtl = 60;
+      maxCacheTtl = 120;
+      
+      defaultCacheTtlSsh = 60;
+      maxCacheTtlSsh = 120;
+      
+
       pinentryFlavor = "gnome3";
     };
   };
