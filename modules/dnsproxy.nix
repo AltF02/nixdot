61e70783a1556b670577601dec8f622ab5af3498
Matthew <contact@altf2.dev>
2023-04-13 21:50:09 +0200

    feat(dnsproxy): add fallback and bootstrap ip

--- modules/dnsproxy.nix
+++ modules/dnsproxy.nix
@@ -16,7 +16,7 @@
       AmbientCapabilities = "cap_net_bind_service";
       NoNewPrivileges = true;
       DynamicUser = true;
-      ExecStart = "${pkgs.dnsproxy}/bin/dnsproxy -l 127.0.0.1 -u quic://${config.networking.hostName}-1bce38.dns.nextdns.io";
+      ExecStart = "${pkgs.dnsproxy}/bin/dnsproxy -l 127.0.0.1 -u quic://${config.networking.hostName}-1bce38.dns.nextdns.io -b 9.9.9.9:53 -f 9.9.9.9:53";
       Restart = "on-failure";
     };
   };
