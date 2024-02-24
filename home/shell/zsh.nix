5bad7d0f3ce4ca9026ac87eb107d4a999dad96ba
Matthew <contact@altf2.dev>
2024-02-24 17:30:35 +0100

    feat(home/programs): add direnv
    Signed-off-by: Matthew <contact@altf2.dev>

--- home/shell/zsh.nix
+++ home/shell/zsh.nix
@@ -42,6 +42,8 @@
 
       # To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
       [[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
+
+      eval "$(direnv hook zsh)"
     '';
 
     shellAliases = {
